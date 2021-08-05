3926917d02c742a443d36fe2af91246c981a8d21
harzh307 <harzh.patel@gmail.com>
2021-08-05 16:23:04 +0530

    reusable button fixed

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -17,7 +17,7 @@ const Button = ({
     <>
       <button
         type="button"
-        className={classNames(!isDisabled && !isLoading && [variant], {
+        className={classNames(!isDisabled && !isLoading && [variant], 'btn', {
           disable: isDisabled || isLoading,
           [className]: !!className,
         })}
@@ -27,119 +27,42 @@ const Button = ({
       </button>
       <style jsx>
         {`
-          .primarySmall {
-            background-color: #064ea4;
+          .btn {
             transition-duration: 300ms;
-            font-size: 16px;
-            color: #f9fafb;
-            line-height: 1.75rem;
-            padding: 20px 32px;
             border-radius: 100px;
             text-transform: uppercase;
             font-family: sans-serif;
             font-weight: 500;
             border: none;
             align-items: center;
+            line-height: 1.175rem;
             cursor: pointer;
+            font-size: 16px;
           }
-          .primarySmall:hover {
+          .primary {
+            background-color: #064ea4;
+            color: #f9fafb;
+          }
+          .primary:hover {
             background-color: #1e3d60;
             transform: scale(0.97);
           }
-          .primaryDefault {
-            background-color: #064ea4;
-            transition-duration: 300ms;
-            font-size: 16px;
-            color: #f9fafb;
-            line-height: 1.75rem;
-            padding: 24px 40px;
-            border-radius: 100px;
-            text-transform: uppercase;
-            font-family: sans-serif;
-            font-weight: 500;
-            border: none;
-            align-items: center;
-            cursor: pointer;
-          }
-          .primaryDefault:hover {
-            background-color: #1e3d60;
-            transform: scale(0.97);
-          }
-          .primaryLarge {
-            background-color: #064ea4;
-            transition-duration: 300ms;
-            font-size: 16px;
-            color: #f9fafb;
-            line-height: 1.75rem;
-            padding: 24px 68px;
-            border-radius: 100px;
-            text-transform: uppercase;
-            font-family: sans-serif;
-            font-weight: 500;
-            border: none;
-            align-items: center;
-            cursor: pointer;
-          }
-          .primaryLarge:hover {
-            background-color: #1e3d60;
-            transform: scale(0.97);
-          }
-          .secondarySmall {
+          .secondary {
             background-color: #e7eaee;
-            transition-duration: 300ms;
-            font-size: 16px;
             color: #1e3d60;
-            line-height: 1.75rem;
-            padding: 20px 32px;
-            border-radius: 100px;
-            text-transform: uppercase;
-            font-family: sans-serif;
-            font-weight: 500;
-            border: none;
-            align-items: center;
-            cursor: pointer;
           }
-          .secondarySmall:hover {
+          .secondary:hover {
             background-color: #becad7;
             transform: scale(0.97);
           }
-          .secondaryDefault {
-            background-color: #e7eaee;
-            transition-duration: 300ms;
-            font-size: 16px;
-            color: #1e3d60;
-            line-height: 1.75rem;
-            padding: 24px 40px;
-            border-radius: 100px;
-            text-transform: uppercase;
-            font-family: sans-serif;
-            font-weight: 500;
-            border: none;
-            align-items: center;
-            cursor: pointer;
+          .small {
+            padding: 16px 30px;
           }
-          .secondaryDefault:hover {
-            background-color: #becad7;
-            transform: scale(0.97);
+          .default {
+            padding: 20px 36px;
           }
-          .secondaryLarge {
-            background-color: #e7eaee;
-            transition-duration: 300ms;
-            font-size: 16px;
-            color: #1e3d60;
-            line-height: 1.75rem;
-            padding: 24px 68px;
-            border-radius: 100px;
-            text-transform: uppercase;
-            font-family: sans-serif;
-            font-weight: 500;
-            border: none;
-            align-items: center;
-            cursor: pointer;
-          }
-          .secondaryLarge:hover {
-            background-color: #becad7;
-            transform: scale(0.97);
+          .large {
+            padding: 22px 48px;
           }
           .buttonIcon {
             background-color: #e0edfb;
@@ -169,19 +92,20 @@ const Button = ({
 
 Button.propTypes = {
   variant: PropTypes.oneOf([
-    'primarySmall',
-    'primaryDefault',
-    'primaryLarge',
-    'secondarySmall',
-    'secondaryDefault',
-    'secondaryLarge',
+    'small',
+    'default',
+    'large',
+    'sSmall',
+    'sDefault',
+    'sLarge',
     'buttonIcon',
     'disable',
+    'btn',
   ]),
 };
 
 Button.defaultProps = {
-  variant: 'primarySmall',
+  variant: 'small',
 };
 
 export default Button;
