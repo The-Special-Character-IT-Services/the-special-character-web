76993c94fc946d1f18715bef2f459f5eac1c80fd
Devanshi Desai <contact.ddevanshi@gmail.com>
2021-08-06 12:00:07 +0530

    JRA-SC-6 #scss applied to card component

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,12 +1,12 @@
 import { Props } from 'react';
-import styles from './card.module.css';
+import styles from './card.module.scss';
 import classNames from 'classnames';
 
 const Card = ({ children, className, ...props }: Props) => {
   return (
     <>
       <div
-        className={classNames(styles.card, className ? className : '')}
+        className={classNames(styles.card, { [className]: !!className })}
         {...props}>
         {children}
       </div>
