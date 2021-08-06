9df481165583f858c9f4646d2d756b5b7ba207e4
Devanshi Desai <contact.ddevanshi@gmail.com>
2021-08-06 12:42:55 +0530

    JRA-SC-7 #Image component with avtar variant

--- components/Images/index.tsx
+++ components/Images/index.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const Images = ({ children, className, alt, ...props }: Props) => {
+const Images = ({ children, className, variant, alt, ...props }: Props) => {
   return (
     <>
-      <div className={classNames('image', className ? className : '')}>
+      <div className={classNames(variant, className ? className : '')}>
         <Image alt={alt} {...props} />
 
         <style jsx>
@@ -15,11 +16,22 @@ const Images = ({ children, className, alt, ...props }: Props) => {
               border-radius: 2rem;
               overflow: hidden;
             }
+            .avtar {
+              display: flex;
+              justify-content: center;
+              border-radius: 50%;
+              overflow: hidden;
+            }
           `}
         </style>
       </div>
     </>
   );
 };
-
+Images.propTypes = {
+  variant: PropTypes.oneOf(['image', 'avtar']),
+};
+Images.defaultProps = {
+  variant: 'image',
+};
 export default Images;
