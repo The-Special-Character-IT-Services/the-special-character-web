3a9fc22c1943eca1e7497699ccffe51881960b15
Devanshi Desai <contact.ddevanshi@gmail.com>
2021-08-09 11:21:01 +0530

    Divider breakpoints

--- components/Divider/index.tsx
+++ components/Divider/index.tsx
@@ -5,14 +5,32 @@ interface Props {}
 const Divider = () => {
   return (
     <>
-      <div />
+      <div className="divider" />
       <style jsx>
         {`
-          div {
+        {/* @import '' */}
+          .divider {
             height: 1px;
             width: 100%;
             opacity: 0.15;
             background-color: #415977;
+          }
+          @media screen and (max-width: 991px) {
+            .divider {
+              margin-top: 3.12rem;
+              margin-bottom: 5rem;
+            }
+          }
+          @media screen and (max-width: 767px) {
+            .divider {
+              margin-top: 2.5rem;
+              margin-bottom: 3.75rem;
+            }
+          }
+          @media screen and (max-width: 479px) {
+            .divider {
+              margin-bottom: 3.12rem;
+            }
           }
         `}
       </style>
