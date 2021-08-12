728379842d44534e9c17640ce15ee85b1790531c
Devanshi Desai <contact.ddevanshi@gmail.com>
2021-08-12 12:37:08 +0530

    JRA-12 #Ratings container created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Banner from 'container/Banner';
 import Courses from 'container/Courses';
+import Ratings from 'container/Ratings';
 
 export default function Home() {
   return (
     <>
       <Banner />
       <Courses />
+      <Ratings />
     </>
   );
 }
