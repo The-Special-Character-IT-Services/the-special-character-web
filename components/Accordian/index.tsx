f38e10eb80b95d4c797acb7c10a99b17c320d00b
Devanshi Desai <contact.ddevanshi@gmail.com>
2021-08-13 16:27:54 +0530

    JRA-SC-12 #Accordian

--- components/Accordian/index.tsx
+++ components/Accordian/index.tsx
@@ -24,14 +24,16 @@ const Accordian = ({ children, className, label, ...props }: Props) => {
       role="button"
       onClick={toggleAcc}
       {...props}>
-      <div className={styles.showDiv}>
+      <div className={styles.buttonDiv}>
         <button className={styles.acBtn} id="toggle" ref={toggleRef} />
+      </div>
+      <div className={styles.showDiv}>
         <Typography variant="h3" id="headTxt">
           {label}
         </Typography>
-      </div>
-      <div className={styles.hideDiv} ref={childRef}>
-        {children}
+        <div className={styles.hideDiv} ref={childRef}>
+          {children}
+        </div>
       </div>
     </Card>
   );
