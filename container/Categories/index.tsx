17d41a9b5adc5aade204299d1ddb101748575030
Devanshi Desai <contact.ddevanshi@gmail.com>
2021-09-01 15:02:59 +0530

    Divider added

--- container/Categories/index.tsx
+++ container/Categories/index.tsx
@@ -1,5 +1,6 @@
 import Button from '@components/Button';
 import Card from '@components/Card';
+import Divider from '@components/Divider';
 import Typography from '@components/Typography';
 import Image from 'next/image';
 import { CourseCategoryType } from 'types';
@@ -11,37 +12,49 @@ interface Props {
 
 const Categories = ({ data }: Props) => {
   return (
-    <section className={styles.categories}>
-      <div className={styles.container}>
-        <Typography variant="h2">{data?.heading?.title}</Typography>
-        <div className={styles.cardDiv}>
-          {data?.cardComponent?.map(x => (
-            <Card key={x.id} variant="cardHover" className={styles.cardBox}>
-              <a href={`/courses/${x.id}`}>
-                <Image
-                  alt="Design Category"
-                  src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${x?.image?.url}`}
-                  height={311}
-                  width={369}
-                />
-                <div className={styles.paraDiv}>
-                  <Typography variant="h3">{x.title}</Typography>
-                  <Typography>{x.description}</Typography>
-                </div>
-              </a>
-            </Card>
-          ))}
+    <>
+      <section className={styles.categories}>
+        <div className={styles.container}>
+          <Divider className={styles.divider} />
+          <Typography variant="h2">{data?.heading?.title}</Typography>
+          <div className={styles.cardDiv}>
+            {data?.cardComponent?.map(x => (
+              <Card key={x.id} variant="cardHover" className={styles.cardBox}>
+                <a href={`/courses/${x.id}`}>
+                  <Image
+                    alt="Design Category"
+                    src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${x?.image?.url}`}
+                    height={400}
+                    width={550}
+                  />
+                  <Button className="secondary">
+                    <Image
+                      alt="Play"
+                      src="https://assets.website-files.com/607de2d8e8911e32707a3efe/607f4d1b6d6b05c4f9a7289c_icon-card-category-courses-education-x-template.svg"
+                      height={12}
+                      width={12}
+                    />
+                    <Typography>12 courses</Typography>
+                  </Button>
+                  <div className={styles.paraDiv}>
+                    <Typography variant="h3">{x.title}</Typography>
+                    <Typography>{x.description}</Typography>
+                  </div>
+                </a>
+              </Card>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className={styles.arrow}>
-        <Image
-          alt="Next Section"
-          src="https://assets.website-files.com/607de2d8e8911e32707a3efe/607f6baa1a6ec59517e0c917_arrow-2-education-x-template.svg"
-          height={206}
-          width={77}
-        />
-      </div>
-    </section>
+        <div className={styles.arrow}>
+          <Image
+            alt="Next Section"
+            src="https://assets.website-files.com/607de2d8e8911e32707a3efe/607f6baa1a6ec59517e0c917_arrow-2-education-x-template.svg"
+            height={206}
+            width={77}
+          />
+        </div>
+      </section>
+    </>
   );
 };
 
